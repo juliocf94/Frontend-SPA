19d538a100809a8bd9bdfbe47cd37bb0f53fb97f
Julio Flores <cesarff.julio@gmail.com>
2024-02-27 14:29:19 -0600

    feat: :art: Descargar reporte
    Se ha implementado la logica para descargar el listado en formato pdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
 import Menu from "./components/Menu";
-import VisitorRegistrationForm from './components/molecules/VisitorRegistrationForm'; // Ruta correcta al archivo del componente
-import ListVisitors from './components/Visitors';
+import VisitorRegistrationForm from "./components/molecules/VisitorRegistrationForm"; // Ruta correcta al archivo del componente
+import ListVisitors from "./components/Visitors";
+import AxiosManager from "./utils/AxiosManager";
 
 function App() {
+  const handleDownloadPDF = () => {
+    const axiosManager = new AxiosManager("http://127.0.0.1:8000/api");
+
+    axiosManager
+      .get("visitors/download-pdf") // Ruta donde se encuentra el endpoint para generar el PDF
+      .then((response) => {
+        // Crear un enlace temporal para descargar el archivo
+        const url = window.URL.createObjectURL(new Blob([response.data]));
+        const link = document.createElement("a");
+        link.href = url;
+        link.setAttribute("download", "visitors.pdf");
+        document.body.appendChild(link);
+        link.click();
+      })
+      .catch((error) => {
+        console.error("Error al descargar el PDF:", error);
+      });
+  };
+
   return (
     <div className="App">
       <Menu />
-      <ListVisitors/>
+      <ListVisitors />
+      <Tooltip title="Descargar PDF" arrow>
+        <Button onClick={handleDownloadPDF} variant="contained" color="primary">Descargar PDF</Button>
+      </Tooltip>
     </div>
   );
 }
